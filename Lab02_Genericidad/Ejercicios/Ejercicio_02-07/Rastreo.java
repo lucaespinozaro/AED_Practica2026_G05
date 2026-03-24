f1c2202f2c994f88475ed575a145b959a1e30d25
saluhera2023 <saluhera2023@gmail.com>
2026-03-24 09:11:02 -0500

    Update Rastreo.java

--- Lab02_Genericidad/Ejercicios/Ejercicio_02-07/Rastreo.java
+++ Lab02_Genericidad/Ejercicios/Ejercicio_02-07/Rastreo.java
@@ -1,1 +1,23 @@
+public class Rastreo<T> {
+    private String color;
+    private int posicion;
+    private T objeto;
 
+    public Rastreo(String color, int posicion, T objeto) {
+        this.color = color;
+        this.posicion = posicion;
+        this.objeto = objeto;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public T getObjeto() {
+        return objeto;
+    }
+}
