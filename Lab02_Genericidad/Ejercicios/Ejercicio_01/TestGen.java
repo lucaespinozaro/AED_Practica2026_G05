0e85157af120494b85022f67849f15b541bf59d9
saluhera2023 <saluhera2023@gmail.com>
2026-03-24 02:25:48 -0500

    Update TestGen.java

--- Lab02_Genericidad/Ejercicios/Ejercicio_01/TestGen.java
+++ Lab02_Genericidad/Ejercicios/Ejercicio_01/TestGen.java
@@ -1,5 +1,4 @@
 public class TestGen {
-
     public static <T> boolean exist(T[] arreglo, T elemento) {
         for (T item : arreglo) {
             if (item.equals(elemento)) {
@@ -10,7 +9,6 @@ public class TestGen {
     }
 
     public static void main(String[] args) {
-
         String[] v = {"Perez", "Sanchez", "Rodriguez"};
         Integer[] w = {12, 34, 56};
 
