fcb072d4a09f7a88e62fa26d33cd2c896c67d8bf
saluhera2023 <saluhera2023@gmail.com>
2026-03-24 00:56:08 -0500

    Create Bolsa.java

---
+++ Lab02_Genericidad/Actividades/Actividad_01-04_07/Bolsa.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Bolsa<T> implements Iterable<T> {
+    private ArrayList<T> lista;
+    private int tope;
+
+    public Bolsa(int tope) {
+        this.tope = tope;
+        this.lista = new ArrayList<>();
+    }
+
+    public void add(T objeto) {
+        if (lista.size() < tope) {
+            lista.add(objeto);
+        } else {
+            throw new RuntimeException("No caben más objetos");
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return lista.iterator();
+    }
+}
