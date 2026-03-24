e93b02005aac16f6ed6aade2db65463f049f4e08
saluhera2023 <saluhera2023@gmail.com>
2026-03-24 07:03:28 -0500

    Update TestGen.java

--- Lab02_Genericidad/Ejercicios/Ejercicio_02-07/TestGen.java
+++ Lab02_Genericidad/Ejercicios/Ejercicio_02-07/TestGen.java
@@ -11,11 +11,11 @@ public class TestGen {
     public static void main(String[] args) {
         Cajoneria<Golosina> cajoneria = new Cajoneria<>(6);
 
-        Caja<Golosina> c1 = new Caja<>("Rojo", new Golosina("Gomitas", 50));
-        Caja<Golosina> c2 = new Caja<>("Amarillo", new Golosina("Caramelo", 20));
-        Caja<Golosina> c3 = new Caja<>("Verde", new Golosina("Chocolate", 30));
-        Caja<Golosina> c4 = new Caja<>("Azul", new Golosina("Gomitas", 50));
-        Caja<Golosina> c5 = new Caja<>("Blanco", new Golosina("Caramelo", 20));
+        Caja<Golosina> c1 = new Caja<>("Rojo", new Golosina("Gomitas", 40));
+        Caja<Golosina> c2 = new Caja<>("Amarillo", new Golosina("Caramelo", 10));
+        Caja<Golosina> c3 = new Caja<>("Verde", new Golosina("Chocolate", 20));
+        Caja<Golosina> c4 = new Caja<>("Azul", new Golosina("Gomitas", 40));
+        Caja<Golosina> c5 = new Caja<>("Blanco", new Golosina("Caramelo", 10));
 
         cajoneria.add(c1);
         cajoneria.add(c2);
@@ -23,21 +23,20 @@ public class TestGen {
         cajoneria.add(c4);
         cajoneria.add(c5);
 
-        Busqueda<Golosina> b = cajoneria.search(new Golosina("Gomitas", 50));
+        Busqueda<Golosina> b = cajoneria.search(new Golosina("Gomitas", 40));
         if (b != null) {
-            System.out.println(b.getPosicion() + " " + b.getColor() + " " + b.getObjeto());
+            System.out.println(b.getPosicion() + " - " + b.getColor() + " - " + b.getObjeto());
         }
 
-        System.out.println("Eliminado: " + cajoneria.delete(new Golosina("Caramelo", 20)));
+        System.out.println("Eliminado: " + cajoneria.delete(new Golosina("Caramelo", 10)));
+        System.out.println("Cantidad de Gomitas: " + cajoneria.contar(new Golosina("Gomitas", 40)));
 
-        System.out.println("Cantidad de Gomitas: " + cajoneria.contar(new Golosina("Gomitas", 50)));
-
-        System.out.println("--- Cajoneria ---");
+        System.out.println("--- Cajoneria Golosinas ---");
         System.out.println(cajoneria);
 
         Cajoneria<Chocolatina> cajoneriaChoco = new Cajoneria<>(3);
-        cajoneriaChoco.add(new Caja<>("Rojo", new Chocolatina("Milka")));
-        cajoneriaChoco.add(new Caja<>("Azul", new Chocolatina("Ferrero")));
+        cajoneriaChoco.add(new Caja<>("Rojo", new Chocolatina("Sublime")));
+        cajoneriaChoco.add(new Caja<>("Azul", new Chocolatina("Mecano")));
 
         System.out.println("--- Cajoneria Chocolatinas ---");
         System.out.println(cajoneriaChoco);
