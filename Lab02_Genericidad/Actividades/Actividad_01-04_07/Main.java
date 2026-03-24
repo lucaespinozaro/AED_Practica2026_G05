9bc4b2a345ce2a7a419cec5d5ef2d5e8ab81ce44
saluhera2023 <saluhera2023@gmail.com>
2026-03-24 00:57:16 -0500

    Update Main.java

--- Lab02_Genericidad/Actividades/Actividad_01-04_07/Main.java
+++ Lab02_Genericidad/Actividades/Actividad_01-04_07/Main.java
@@ -1,1 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
 
+        Bolsa<Chocolatina> bolsaChoco = new Bolsa<>(3);
+        bolsaChoco.add(new Chocolatina("Milka"));
+        bolsaChoco.add(new Chocolatina("Ferrero"));
+        bolsaChoco.add(new Chocolatina("Lindt"));
+
+        for (Chocolatina c : bolsaChoco) {
+            System.out.println(c.getMarca());
+        }
+
+        System.out.println("-----");
+
+        Bolsa<Golosina> bolsaGolo = new Bolsa<>(4);
+        bolsaGolo.add(new Golosina("Gomitas", 50));
+        bolsaGolo.add(new Golosina("Caramelo", 20));
+        bolsaGolo.add(new Golosina("Chocolate", 30));
+
+        for (Golosina g : bolsaGolo) {
+            System.out.println(g.getNombre() + " - " + g.getPeso());
+        }
+    }
+}
