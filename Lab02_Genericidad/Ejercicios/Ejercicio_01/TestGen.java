065b46b651cd0f274cd56653a5f5926d537522b7
saluhera2023 <saluhera2023@gmail.com>
2026-03-24 01:24:43 -0500

    Create TestGen.java

---
+++ Lab02_Genericidad/Ejercicios/Ejercicio_01/TestGen.java
@@ -0,0 +1,33 @@
+public class TestGen {
+
+    public static <T> boolean exist(T[] arreglo, T elemento) {
+        for (T item : arreglo) {
+            if (item.equals(elemento)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        String[] v = {"Perez", "Sanchez", "Rodriguez"};
+        Integer[] w = {12, 34, 56};
+
+        System.out.println(exist(v, "Sanchez"));
+        System.out.println(exist(w, 34));
+
+        Golosina[] g = {
+            new Golosina("Gomitas", 50),
+            new Golosina("Caramelo", 20)
+        };
+
+        Chocolatina[] c = {
+            new Chocolatina("Milka"),
+            new Chocolatina("Ferrero")
+        };
+
+        System.out.println(exist(g, new Golosina("Gomitas", 50)));
+        System.out.println(exist(c, new Chocolatina("Milka")));
+    }
+}
