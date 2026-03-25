6e8bd16bda11ec3d8b74f4e2e5bc31f916ceae7e
saluhera2023 <saluhera2023@gmail.com>
2026-03-25 14:03:46 -0500

    Update Chocolatina.java

--- Lab02_Genericidad/Ejercicios/Ejercicio_02-07/Chocolatina.java
+++ Lab02_Genericidad/Ejercicios/Ejercicio_02-07/Chocolatina.java
@@ -1,6 +1,5 @@
-public class Chocolatina {
+public class Chocolatina implements Comparable<Chocolatina> {
     private String marca;
-
     public Chocolatina(String marca) {
         this.marca = marca;
     }
@@ -16,9 +15,25 @@ public class Chocolatina {
 
     @Override
     public boolean equals(Object obj) {
+        if (obj == null) return false; 
         if (this == obj) return true;
         if (!(obj instanceof Chocolatina)) return false;
         Chocolatina c = (Chocolatina) obj;
+
+        if (marca == null || c.marca == null) {
+            return marca == c.marca;
+        }
+
         return marca.equals(c.marca);
+    }
+
+    @Override
+    public int compareTo(Chocolatina o) {
+        if (o == null) return 1;
+        if (this.marca == null && o.marca == null) return 0;
+        if (this.marca == null) return -1;
+        if (o.marca == null) return 1;
+
+        return this.marca.compareTo(o.marca);
     }
 }
