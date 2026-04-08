841ffc8cac807bdaace32c6e12e0a695fbeca794
saluhera2023 <saluhera2023@gmail.com>
2026-04-07 23:50:18 -0500

    Update ConteoI.java

--- Lab03_Complejidad_Algoritmica/Ejercicios/Ejercicio_02/ConteoI.java
+++ Lab03_Complejidad_Algoritmica/Ejercicios/Ejercicio_02/ConteoI.java
@@ -1,6 +1,6 @@
 FUNCIÓN ConteoI (v : VECTOR(ENTERO), n : ENTERO) : ENTERO
     conteo := 0                         // O(1)
-    PARA i DESDE 0 HASTA n-2            // O(n) -> O(n) * O(n) = |$O(n^2)$|
+    PARA i DESDE 0 HASTA n-2            // O(n) -> O(n) * O(n) = O(n^2)
         PARA j DESDE i+1 HASTA n-1          // O(n)
             SI v[i] = v[j]                      // O(1)
                 conteo := conteo + 1                // O(1)
