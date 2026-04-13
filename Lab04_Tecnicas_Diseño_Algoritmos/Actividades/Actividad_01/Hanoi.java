765c8c278d02fd01a748514fbb645e61a746f3fc
jhazielg <129285104+jhazielg@users.noreply.github.com>
2026-04-13 17:49:19 -0500

    Update and rename Main.java to Hanoi.java

---
+++ Lab04_Tecnicas_Diseño_Algoritmos/Actividades/Actividad_01/Hanoi.java
@@ -0,0 +1,20 @@
+package Actividades;
+public class Hanoi {
+
+    public static void main(String[] args) {
+        int numDiscos = 3;
+        System.out.println(" Torres de Hanoi con " + numDiscos + " discos ");
+        torresHanoi(numDiscos, 1, 2, 3);
+        System.out.println("Total de movimientos: " + (int)(Math.pow(2, numDiscos) - 1));
+    }
+
+    public static void torresHanoi(int discos, int origen, int auxiliar, int destino) {
+        if (discos == 1) {
+            System.out.println("Mover disco 1 de Torre " + origen + " a Torre " + destino);
+        } else {
+            torresHanoi(discos - 1, origen, destino, auxiliar);
+            System.out.println("Mover disco " + discos + " de Torre " + origen + " a Torre " + destino);
+            torresHanoi(discos - 1, auxiliar, origen, destino);
+        }
+    }
+}
