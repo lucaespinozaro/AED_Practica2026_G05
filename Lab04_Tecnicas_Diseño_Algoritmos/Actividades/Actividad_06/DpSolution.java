cf6c333bfcdea1612c7e879bbbf8ddba1ea9e8d5
saluhera2023 <saluhera2023@gmail.com>
2026-04-21 06:26:52 -0500

    Create DpSolution.java

---
+++ Lab04_Tecnicas_Diseño_Algoritmos/Actividades/Actividad_06/DpSolution.java
@@ -0,0 +1,23 @@
+public class DpSolution {
+    static int getValue(int[] values, int rodLength) {
+        // subSolutions[i] guarda el máximo valor para una varilla de largo i
+        int[] subSolutions = new int[rodLength + 1];
+        subSolutions[0] = 0; // varilla de largo 0 vale nada
+ 
+        for (int i = 1; i <= rodLength; i++) {
+            int tmpMax = -1;
+            for (int j = 0; j < i; j++) {
+                tmpMax = Math.max(tmpMax,
+                                  values[j] + subSolutions[i - j - 1]);
+            }
+            subSolutions[i] = tmpMax; 
+ 
+        return subSolutions[rodLength];
+    }
+ 
+    public static void main(String[] args) {
+        int[] values  = {3, 7, 1, 3, 9};
+        int rodLength = values.length;
+        System.out.println("El valor maximo: " + getValue(values, rodLength));
+    }
+}
