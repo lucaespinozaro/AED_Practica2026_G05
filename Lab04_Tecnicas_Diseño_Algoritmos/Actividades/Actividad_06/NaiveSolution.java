ef7f0647e013c0f56281d11e18666717ec441858
saluhera2023 <saluhera2023@gmail.com>
2026-04-21 06:27:12 -0500

    Create NaiveSolution.java

---
+++ Lab04_Tecnicas_Diseño_Algoritmos/Actividades/Actividad_06/NaiveSolution.java
@@ -0,0 +1,21 @@
+public class NaiveSolution {
+    static int getValue(int[] values, int length) {
+        if (length <= 0)
+            return 0;
+ 
+        int tmpMax = -1;
+        for (int i = 0; i < length; i++) {
+            // Probamos cortar un trozo de tamaño (i+1) y resolver el resto
+            tmpMax = Math.max(tmpMax,
+                              values[i] + getValue(values, length - i - 1));
+        }
+        return tmpMax;
+    }
+ 
+    public static void main(String[] args) {
+        int[] values   = {3, 7, 1, 3, 9};
+        int rodLength  = values.length;
+        System.out.println("El valor maximo: " + getValue(values, rodLength));
+        // Salida: El valor maximo: 17
+    }
+}
