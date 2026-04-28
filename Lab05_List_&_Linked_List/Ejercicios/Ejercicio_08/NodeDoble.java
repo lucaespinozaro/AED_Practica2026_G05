903a26128309d8ac3262e868daa31f7330408301
saluhera2023 <saluhera2023@gmail.com>
2026-04-28 02:31:36 -0500

    Update NodeDoble.java

--- Lab05_List_&_Linked_List/Ejercicios/Ejercicio_08/NodeDoble.java
+++ Lab05_List_&_Linked_List/Ejercicios/Ejercicio_08/NodeDoble.java
@@ -1,11 +1,9 @@
-class NodeDoble<T>
-{
+class NodeDoble<T> {
     T data;
     NodeDoble<T> next;
     NodeDoble<T> prev;
 
-    public NodeDoble(T data)
-  {
+    public NodeDoble(T data) {
         this.data = data;
         this.next = null;
         this.prev = null;
