34b493703da7d6839908d4a2706a197d0484f420
saluhera2023 <saluhera2023@gmail.com>
2026-04-27 22:35:34 -0500

    Update SortedListLinked.java

--- Lab05_List_&_Linked_List/Ejercicios/Ejercicio_01-07/SortedListLinked.java
+++ Lab05_List_&_Linked_List/Ejercicios/Ejercicio_01-07/SortedListLinked.java
@@ -1,6 +1,24 @@
 public class SortedListLinked<T extends Comparable<T>> extends ListLinked<T> {
     public void insertOrden(T x) {
         if (x == null) throw new IllegalArgumentException("Dato null");
-        insertOrdered(x);
+
+        Node<T> nuevo = new Node<>(x);
+        Node<T> first = getFirstNode();
+
+        if (first == null || first.dato.compareTo(x) >= 0) {
+            nuevo.next = first;
+            setFirstNode(nuevo);
+            incrementSize();
+            return;
+        }
+
+        Node<T> aux = first;
+        while (aux.next != null && aux.next.dato.compareTo(x) < 0) {
+            aux = aux.next;
+        }
+
+        nuevo.next = aux.next;
+        aux.next = nuevo;
+        incrementSize();
     }
 }
