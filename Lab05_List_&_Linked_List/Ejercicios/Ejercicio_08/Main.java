92e6ca55b2fda7aaeb1e9ec970ed96a44ce31570
saluhera2023 <saluhera2023@gmail.com>
2026-04-28 02:32:48 -0500

    Update Main.java

--- Lab05_List_&_Linked_List/Ejercicios/Ejercicio_08/Main.java
+++ Lab05_List_&_Linked_List/Ejercicios/Ejercicio_08/Main.java
@@ -1,35 +1,37 @@
-public class Main
-{
-  public static void main(String[] args)
-  {
-    ColaReproduccion<Cancion> cola = new ColaReproduccion<>();
+public class Main {
+    public static void main(String[] args) {
+        ColaReproduccion<Cancion> cola = new ColaReproduccion<>();
 
-    cola.agregarCancion(new Cancion("Bohemian Rhapsody", "Queen", 354));
-    cola.agregarCancion(new Cancion("Blinding Lights", "The Weeknd", 200));
-    cola.agregarCancion(new Cancion("Shape of You", "Ed Sheeran", 234));
-    cola.agregarCancion(new Cancion("Smells Like Teen Spirit", "Nirvana", 301));
-    cola.agregarCancion(new Cancion("Rolling in the Deep", "Adele", 228));
-    cola.agregarCancion(new Cancion("Hotel California", "Eagles", 391));
+        cola.agregarCancion(new Cancion("Bohemian Rhapsody", "Queen", 354));
+        cola.agregarCancion(new Cancion("Blinding Lights", "The Weeknd", 200));
+        cola.agregarCancion(new Cancion("Shape of You", "Ed Sheeran", 234));
+        cola.agregarCancion(new Cancion("Smells Like Teen Spirit", "Nirvana", 301));
+        cola.agregarCancion(new Cancion("Rolling in the Deep", "Adele", 228));
+        cola.agregarCancion(new Cancion("Hotel California", "Eagles", 391));
 
-    System.out.println("=== Cola de Reproducción Inicial ===");
-    cola.mostrarCola();
+        System.out.println("=== Cola de Reproducción Inicial ===");
+        cola.mostrarCola();
 
-    System.out.println("\nAvanzando 3 canciones...");
-    for (int i = 0; i < 3; i++) {
-      System.out.println("► " + cola.reproducirSiguiente());
+        System.out.println("\nAvanzando 3 canciones...");
+        for (int i = 0; i < 3; i++) {
+            Cancion c = cola.reproducirSiguiente();
+            if (c != null)
+                System.out.println("► " + c);
+        }
+
+        System.out.println("\nRetrocediendo 1 canción...");
+        Cancion anterior = cola.reproducirAnterior();
+        if (anterior != null)
+            System.out.println("◄ " + anterior);
+
+        System.out.println("\n=== Mezclando... ===");
+        cola.mezclar();
+        cola.mostrarCola();
+
+        int total = cola.duracionTotal();
+        int min = total / 60;
+        int seg = total % 60;
+
+        System.out.printf("\nDuración total: %d:%02d\n", min, seg);
     }
-
-    System.out.println("\nRetrocediendo 1 canción...");
-    System.out.println("◄ " + cola.reproducirAnterior());
-
-    System.out.println("\n=== Mezclando... ===");
-    cola.mezclar();
-    cola.mostrarCola();
-
-    int total = cola.duracionTotal();
-    int minutos = total / 60;
-    int segundos = total % 60;
-
-    System.out.println("\nDuración total: " + minutos + ":" + segundos);
-  }
 }
