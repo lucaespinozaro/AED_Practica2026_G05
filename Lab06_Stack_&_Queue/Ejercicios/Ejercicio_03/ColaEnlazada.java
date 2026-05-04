a286b4e0d324881975758c35e97240e69bcda200
saluhera2023 <saluhera2023@gmail.com>
2026-05-04 17:44:03 -0500

    Create ColaEnlazada.java

---
+++ Lab06_Stack_&_Queue/Ejercicios/Ejercicio_03/ColaEnlazada.java
@@ -0,0 +1,37 @@
+package Ejercicio_03;
+
+public class ColaEnlazada<E> {
+    private Node<E> inicio;
+    private Node<E> fin;
+
+    public ColaEnlazada() {
+        this.inicio = null;
+        this.fin = null;
+    }
+
+    public boolean isEmpty() {
+        return inicio == null;
+    }
+
+    public void enqueue(E x) {
+        Node<E> n = new Node<>(x);
+        if (isEmpty()) {
+            inicio = fin = n;
+        } else {
+            fin.next = n;
+            fin = n;
+        }
+    }
+
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Cola vacia");
+        }
+        E d = inicio.dato;
+        inicio = inicio.next;
+        if (inicio == null) {
+            fin = null;
+        }
+        return d;
+    }
+}
