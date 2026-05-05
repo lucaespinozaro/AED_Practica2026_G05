f493c8adcb2d4edfa8089e6627a00fe676b9880f
saluhera2023 <saluhera2023@gmail.com>
2026-05-05 03:36:20 -0500

    Update PriorityQueueLinkSort.java

--- Lab06_Stack_&_Queue/Actividades/actividad3/PriorityQueueLinkSort.java
+++ Lab06_Stack_&_Queue/Actividades/actividad3/PriorityQueueLinkSort.java
@@ -1,12 +1,14 @@
 package actividad3;
+
 import actividad1.ExceptionIsEmpty;
 
-public class PriorityQueueLinkSort<E, N extends Comparable<N>> implements PriorityQueue<E, N>
+class PriorityQueueLinkSort<E, N extends Comparable<N>> implements PriorityQueue<E, N>
 {
   class EntryNode
   {
     E data;
     N priority;
+
     EntryNode(E data, N priority) {
       this.data = data;
       this.priority = priority;
@@ -18,48 +20,57 @@ public class PriorityQueueLinkSort<E, N extends Comparable<N>> implements Priori
 
   public PriorityQueueLinkSort()
   {
-    this.first = null;
-    this.last = null;
+    first = null;
+    last = null;
   }
 
   public void enqueue(E x, N pr)
   {
+    if (x == null || pr == null) throw new IllegalArgumentException();
+
     EntryNode nuevoDato = new EntryNode(x, pr);
     Node<EntryNode> nuevoNodo = new Node<>(nuevoDato);
-        
+
     if (isEmpty() || pr.compareTo(first.getData().priority) > 0) {
       nuevoNodo.setNext(first);
       first = nuevoNodo;
       if (last == null) last = first;
     } else {
       Node<EntryNode> temp = first;
-      while (temp.getNext() != null && temp.getNext().getData().priority.compareTo(pr) >= 0) {
+
+      while (temp.getNext() != null &&
+             temp.getNext().getData().priority.compareTo(pr) >= 0) {
         temp = temp.getNext();
       }
+
       nuevoNodo.setNext(temp.getNext());
       temp.setNext(nuevoNodo);
+
       if (nuevoNodo.getNext() == null) last = nuevoNodo;
     }
   }
 
   public E dequeue() throws ExceptionIsEmpty
   {
-    if (isEmpty()) throw new ExceptionIsEmpty("Queue is empty");
-    E aux = this.first.getData().data;
-    this.first = this.first.getNext();
-    if (this.first == null) this.last = null;
+    if (isEmpty()) throw new ExceptionIsEmpty("La cola esta vacia");
+
+    E aux = first.getData().data;
+    first = first.getNext();
+
+    if (first == null) last = null;
+
     return aux;
   }
 
   public E front() throws ExceptionIsEmpty
   {
-    if (isEmpty()) throw new ExceptionIsEmpty("Vacio");
+    if (isEmpty()) throw new ExceptionIsEmpty("La cola esta vacia");
     return first.getData().data;
   }
 
   public E back() throws ExceptionIsEmpty
   {
-    if (isEmpty()) throw new ExceptionIsEmpty("Vacio");
+    if (isEmpty()) throw new ExceptionIsEmpty("La cola esta vacia");
     return last.getData().data;
   }
 
@@ -68,13 +79,20 @@ public class PriorityQueueLinkSort<E, N extends Comparable<N>> implements Priori
     return first == null;
   }
 
-  public String toString() {
-    String res = "PriorityQueue: ";
+  public String toString()
+  {
+    StringBuilder sb = new StringBuilder("PriorityQueue: ");
     Node<EntryNode> temp = first;
+
     while (temp != null) {
-      res += "(" + temp.getData().data + ", P:" + temp.getData().priority + ") ";
+      sb.append("(")
+        .append(temp.getData().data)
+        .append(", P:")
+        .append(temp.getData().priority)
+        .append(") ");
       temp = temp.getNext();
     }
-    return res;
+
+    return sb.toString();
   }
 }
