dface9005290d57255bf59312e8f3f253280cf82
saluhera2023 <saluhera2023@gmail.com>
2026-05-05 06:49:36 -0500

    Update Prioridad.java

--- Lab06_Stack_&_Queue/Adicional/Prioridad.java
+++ Lab06_Stack_&_Queue/Adicional/Prioridad.java
@@ -1,1 +1,16 @@
+public enum Prioridad {
+    VERDE(0, "Verde - Leve"),
+    AMARILLO(1, "Amarillo - Moderado"),
+    ROJO(2, "Rojo - Crítico");
 
+    private final int nivel;
+    private final String descripcion;
+
+    Prioridad(int nivel, String descripcion) {
+        this.nivel = nivel;
+        this.descripcion = descripcion;
+    }
+
+    public int getNivel() { return nivel; }
+    public String getDescripcion() { return descripcion; }
+}
