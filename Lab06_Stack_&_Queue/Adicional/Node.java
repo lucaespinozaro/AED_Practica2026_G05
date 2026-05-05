8784321a3c0d13b9b33d2d6471f8f7e14baf1410
saluhera2023 <saluhera2023@gmail.com>
2026-05-05 06:48:47 -0500

    Update Node.java

--- Lab06_Stack_&_Queue/Adicional/Node.java
+++ Lab06_Stack_&_Queue/Adicional/Node.java
@@ -1,1 +1,14 @@
+public class Node<E> {
+    private E data;
+    public Node<E> next;
 
+    public Node(E data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public E getData() { return data; }
+    public void setData(E data) { this.data = data; }
+    public Node<E> getNext() { return next; }
+    public void setNext(Node<E> next) { this.next = next; }
+}
