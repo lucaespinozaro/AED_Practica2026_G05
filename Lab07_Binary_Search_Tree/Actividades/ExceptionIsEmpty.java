621c6e117f3d569bc6f01bf4f3a7b588784d988e
saluhera2023 <saluhera2023@gmail.com>
2026-05-11 11:25:50 -0500

    Update ExceptionIsEmpty.java

--- Lab07_Binary_Search_Tree/Actividades/ExceptionIsEmpty.java
+++ Lab07_Binary_Search_Tree/Actividades/ExceptionIsEmpty.java
@@ -1,1 +1,9 @@
+public class ExceptionIsEmpty extends Exception {
+    public ExceptionIsEmpty(String msg) {
+        super(msg);
+    }
 
+    public ExceptionIsEmpty() {
+        super();
+    }
+}
