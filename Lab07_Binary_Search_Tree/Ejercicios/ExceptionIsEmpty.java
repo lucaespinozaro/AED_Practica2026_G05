2d679e64297757b61d113290284a213a1b9832cc
lucaespinozaro <luca.espinoza@estudiante.ucsm.edu.pe>
2026-05-11 00:12:04 -0500

    feat(Lab07): Creadas las interfaces y Exceptions de los Ejercicios

---
+++ Lab07_Binary_Search_Tree/Ejercicios/ExceptionIsEmpty.java
@@ -0,0 +1,5 @@
+class ExceptionIsEmpty extends Exception
+{
+  public ExceptionIsEmpty(String msg) {super(msg);}
+  public ExceptionIsEmpty() { super(); }
+}
---
+++ Lab07_Binary_Search_Tree/Ejercicios/ItemDuplicated.java
@@ -0,0 +1,6 @@
+class ItemDuplicated extends Exception
+{
+  public ItemDuplicated(String msg) { super(msg);}
+  public ItemDuplicated() { super(); }
+}
+ 
