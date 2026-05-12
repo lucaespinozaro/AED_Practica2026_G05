d4137b41f6e6bf078c300e9d264a5dbf97cd9e1e
saluhera2023 <saluhera2023@gmail.com>
2026-05-11 21:04:00 -0500

    Update LinkedQueue.java

--- Lab07_Binary_Search_Tree/Ejercicios/LinkedQueue.java
+++ Lab07_Binary_Search_Tree/Ejercicios/LinkedQueue.java
@@ -1,46 +1,50 @@
 public class LinkedQueue<E> {
     private QueueNode<E> front;
     private QueueNode<E> rear;
+    private int size;
 
     public LinkedQueue() {
         this.front = null;
         this.rear = null;
+        this.size = 0;
     }
 
     public boolean isEmpty() {
         return this.front == null;
     }
 
+    public int size() {
+        return this.size;
+    }
+
     public void enqueue(E data) {
-        if (data == null) {
-            throw new IllegalArgumentException("Data null");
-        }
-
+        if (data == null) throw new IllegalArgumentException("Data null");
         QueueNode<E> node = new QueueNode<>(data);
-
         if (isEmpty()) {
             this.front = node;
             this.rear = node;
-            return;
+        } else {
+            this.rear.next = node;
+            this.rear = node;
         }
-
-        this.rear.next = node;
-        this.rear = node;
+        this.size++;
     }
 
     public E dequeue() throws ExceptionIsEmpty {
-        if (isEmpty()) {
-            throw new ExceptionIsEmpty("Queue empty");
-        }
-
+        if (isEmpty()) throw new ExceptionIsEmpty("Queue empty");
         E data = this.front.data;
-
         this.front = this.front.next;
-
-        if (this.front == null) {
-            this.rear = null;
-        }
-
+        if (this.front == null) this.rear = null;
+        this.size--;
         return data;
+    }
+}
+
+class QueueNode<E> {
+    E data;
+    QueueNode<E> next;
+    public QueueNode(E data) {
+        this.data = data;
+        this.next = null;
     }
 }
