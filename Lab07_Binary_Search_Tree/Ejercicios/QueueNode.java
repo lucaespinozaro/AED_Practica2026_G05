f32055bf8dc7c6171af87152a7f398abe34e9ed1
saluhera2023 <saluhera2023@gmail.com>
2026-05-11 20:41:06 -0500

    Update QueueNode.java

--- Lab07_Binary_Search_Tree/Ejercicios/QueueNode.java
+++ Lab07_Binary_Search_Tree/Ejercicios/QueueNode.java
@@ -1,1 +1,13 @@
+class QueueNode<E> {
+    E data;
+    QueueNode<E> next;
 
+    public QueueNode(E data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Data null");
+        }
+
+        this.data = data;
+        this.next = null;
+    }
+}
