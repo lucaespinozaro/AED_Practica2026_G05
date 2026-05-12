b48208b186c7f0f7a6fc24efcfcccb427d84e52e
saluhera2023 <saluhera2023@gmail.com>
2026-05-11 20:45:57 -0500

    Update BinarySearchTree.java

--- Lab07_Binary_Search_Tree/Ejercicios/BinarySearchTree.java
+++ Lab07_Binary_Search_Tree/Ejercicios/BinarySearchTree.java
@@ -1,8 +1,6 @@
-public interface BinarySearchTree<E>
-{
-  void insert(E data) throws ItemDuplicated;
-  E search(E data) throws ItemNotFound;
-  void delete(E data) throws ExceptionIsEmpty;
-  boolean isEmpty();
+public interface BinarySearchTree<E extends Comparable<E>> {
+    void insert(E data) throws ItemDuplicated;
+    E search(E data) throws ItemNotFound;
+    void delete(E data) throws ExceptionIsEmpty, ItemNotFound;
+    boolean isEmpty();
 }
- 
