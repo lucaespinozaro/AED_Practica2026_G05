5096b49f5422185ab1dc9034f789ae32e26d0c0f
saluhera2023 <saluhera2023@gmail.com>
2026-05-17 17:18:14 -0500

    feat(Act3 y Ejer3-6): Añadido la Actividad 3 y dentro incluye los ejercicios 3 al 6

--- Lab08_Tree_AVL/Actividades/AVLTree.java
+++ Lab08_Tree_AVL/Actividades/AVLTree.java
@@ -1,223 +1,270 @@
-package avltree;
-
 public class AVLTree<E extends Comparable<E>> extends BSTree<E> {
-  private boolean heightChanged;
+    private boolean height;
+    
+    class NodeAVL extends Node<E> {
+        protected int bf; 
 
-  public void insert(E data) {
-    root = insertRec((NodeAVL<E>) root, data);
-  }
+        public NodeAVL(E data) {
+            super(data);
+            this.bf = 0;
+        }
 
-  private NodeAVL<E> insertRec(NodeAVL<E> node, E data) {
-    if (node == null) {
-      heightChanged = true;
-      return new NodeAVL<>(data);
+        @Override
+        public String toString() {
+            return data.toString() + "(" + bf + ")";
+        }
     }
 
-    int cmp = data.compareTo(node.data);
-    if (cmp < 0) {
-      node.left = insertRec((NodeAVL<E>) node.left, data);
-      if (heightChanged) {
+    public void insert(E x) throws ItemDuplicated {
+        this.height = false;
+        this.root = insert(x, (NodeAVL) this.root);
+    }
+
+    protected Node<E> insert(E x, NodeAVL node) throws ItemDuplicated {
+        NodeAVL fat = node;
+
+        if (node == null) {
+            this.height = true;
+            fat = new NodeAVL(x);
+        } else {
+            int resC = node.data.compareTo(x);
+            
+            if (resC == 0) {
+                throw new ItemDuplicated(x + " ya se encuentra en el arbol...");
+            }
+
+            if (resC > 0) { 
+                fat.left = insert(x, (NodeAVL) node.left);
+                if (this.height) {
+                    switch (fat.bf) {
+                        case 1:
+                            fat.bf = 0;
+                            this.height = false;
+                            break;
+                        case 0:
+                            fat.bf = -1;
+                            this.height = true;
+                            break;
+                        case -1:
+                            fat = balanceToRight(fat);
+                            this.height = false;
+                            break;
+                    }
+                }
+            } else { 
+                fat.right = insert(x, (NodeAVL) node.right);
+                if (this.height) {
+                    switch (fat.bf) {
+                        case -1:
+                            fat.bf = 0;
+                            this.height = false;
+                            break;
+                        case 0:
+                            fat.bf = 1;
+                            this.height = true;
+                            break;
+                        case 1:
+                            fat = balanceToLeft(fat);
+                            this.height = false;
+                            break;
+                    }
+                }
+            }
+        }
+        return fat;
+    }
+
+    private NodeAVL balanceToLeft(NodeAVL node) {
+        NodeAVL hijo = (NodeAVL) node.right;
+        switch (hijo.bf) {
+            case 1:
+                node.bf = 0;
+                hijo.bf = 0;
+                node = rotateSL(node);
+                break;
+            case -1:
+                NodeAVL nieto = (NodeAVL) hijo.left;
+                switch (nieto.bf) {
+                    case -1: node.bf = 0; hijo.bf = 1; break;
+                    case 0:  node.bf = 0; hijo.bf = 0; break;
+                    case 1:  node.bf = -1; hijo.bf = 0; break;
+                }
+                nieto.bf = 0;
+                node.right = rotateSR(hijo);
+                node = rotateSL(node);
+                break;
+            case 0:
+                node.bf = 1;
+                hijo.bf = -1;
+                node = rotateSL(node);
+                break;
+        }
+        return node;
+    }
+
+    private NodeAVL balanceToRight(NodeAVL node) {
+        NodeAVL hijo = (NodeAVL) node.left;
+        switch (hijo.bf) {
+            case -1:
+                node.bf = 0;
+                hijo.bf = 0;
+                node = rotateSR(node);
+                break;
+            case 1:
+                NodeAVL nieto = (NodeAVL) hijo.right;
+                switch (nieto.bf) {
+                    case 1:  node.bf = 0; hijo.bf = -1; break;
+                    case 0:  node.bf = 0; hijo.bf = 0; break;
+                    case -1: node.bf = 1; hijo.bf = 0; break;
+                }
+                nieto.bf = 0;
+                node.left = rotateSL(hijo);
+                node = rotateSR(node);
+                break;
+            case 0:
+                node.bf = -1;
+                hijo.bf = 1;
+                node = rotateSR(node);
+                break;
+        }
+        return node;
+    }
+
+    private NodeAVL rotateSL(NodeAVL node) {
+        NodeAVL p = (NodeAVL) node.right;
+        node.right = p.left;
+        p.left = node;
+        node = p;
+        return node;
+    }
+
+    private NodeAVL rotateSR(NodeAVL node) {
+        NodeAVL p = (NodeAVL) node.left;
+        node.left = p.right;
+        p.right = node;
+        node = p;
+        return node;
+    }
+
+    public void remove(E x) {
+        this.height = false;
+        this.root = remove(x, (NodeAVL) this.root);
+    }
+
+    protected Node<E> remove(E x, NodeAVL node) {
+        if (node == null) {
+            this.height = false;
+            return null;
+        }
+
+        int cmp = x.compareTo(node.data);
+        if (cmp < 0) {
+            node.left = remove(x, (NodeAVL) node.left);
+            if (this.height) {
+                node = balanceOnRemoveLeft(node);
+            }
+        } else if (cmp > 0) {
+            node.right = remove(x, (NodeAVL) node.right);
+            if (this.height) {
+                node = balanceOnRemoveRight(node);
+            }
+        } else {
+            if (node.left == null || node.right == null) {
+                this.height = true;
+                return (node.left != null) ? node.left : node.right;
+            } else {
+                NodeAVL successor = getMin((NodeAVL) node.right);
+                node.data = successor.data;
+                node.right = remove(successor.data, (NodeAVL) node.right);
+                if (this.height) {
+                    node = balanceOnRemoveRight(node);
+                }
+            }
+        }
+        return node;
+    }
+
+    private NodeAVL getMin(NodeAVL node) {
+        while (node.left != null) {
+            node = (NodeAVL) node.left;
+        }
+        return node;
+    }
+
+    private NodeAVL balanceOnRemoveLeft(NodeAVL node) {
         switch (node.bf) {
-        case 1: node.bf = 0; heightChanged = false; break;
-        case 0: node.bf = -1; break;
-        case -1: node = balanceToRight(node); heightChanged = false; break;
+            case -1: node.bf = 0; this.height = true; break;
+            case 0:  node.bf = 1; this.height = false; break;
+            case 1:
+                NodeAVL rightChild = (NodeAVL) node.right;
+                int bfr = rightChild.bf;
+                node = balanceToLeft(node);
+                this.height = (bfr != 0);
+                break;
         }
-      }
-    } else if (cmp > 0) {
-      node.right = insertRec((NodeAVL<E>) node.right, data);
-      if (heightChanged) {
+        return node;
+    }
+
+    private NodeAVL balanceOnRemoveRight(NodeAVL node) {
         switch (node.bf) {
-        case -1: node.bf = 0; heightChanged = false; break;
-        case 0: node.bf = 1; break;
-        case 1: node = balanceToLeft(node); heightChanged = false; break;
+            case 1:  node.bf = 0; this.height = true; break;
+            case 0:  node.bf = -1; this.height = false; break;
+            case -1:
+                NodeAVL leftChild = (NodeAVL) node.left;
+                int bfl = leftChild.bf;
+                node = balanceToRight(node);
+                this.height = (bfl != 0);
+                break;
         }
-      }
-    } else {
-      heightChanged = false;
-    }
-    return node;
-  }
-  //-------------------------------------------------------
-  //EJERCICIO 3
-  //-------------------------------------------------------  
-  public void remove(E data) {
-    root = removeRec((NodeAVL<E>) root, data);
-  }
-
-  private NodeAVL<E> removeRec(NodeAVL<E> node, E data) {
-    if (node == null) {
-      heightChanged = false;
-      return null;
+        return node;
     }
 
-    int cmp = data.compareTo(node.data);
-    if (cmp < 0) {
-      node.left = removeRec((NodeAVL<E>) node.left, data);
-      if (heightChanged) {
-        node = balanceOnRemoveLeft(node);
-      }
-    } else if (cmp > 0) {
-      node.right = removeRec((NodeAVL<E>) node.right, data);
-      if (heightChanged) {
-        node = balanceOnRemoveRight(node);
-      }
-    } else {
-      if (node.left == null || node.right == null) {
-        heightChanged = true;
-        return (NodeAVL<E>) (node.left != null ? node.left : node.right);
-      } else {
-        NodeAVL<E> successor = getMin((NodeAVL<E>) node.right);
-        node.data = successor.data;
-        node.right = removeRec((NodeAVL<E>) node.right, successor.data);
-        if (heightChanged) {
-          node = balanceOnRemoveRight(node);
+    public void breadthFirst() {
+        int h = height((NodeAVL) root);
+        for (int i = 0; i < h; i++) { 
+            printGivenLevel((NodeAVL) root, i);
         }
-      }
+        System.out.println();
     }
-    return node;
-  }
 
-  private NodeAVL<E> getMin(NodeAVL<E> node) {
-    while (node.left != null) node = (NodeAVL<E>) node.left;
-    return node;
-  }
-
-  private NodeAVL<E> balanceOnRemoveLeft(NodeAVL<E> node) {
-    switch (node.bf) {
-    case -1: node.bf = 0; break;
-    case 0: node.bf = 1; heightChanged = false; break;
-    case 1:
-      NodeAVL<E> rightChild = (NodeAVL<E>) node.right;
-      int bfr = rightChild.bf;
-      node = balanceToLeft(node);
-      if (bfr == 0) heightChanged = false;
-      break;
+    private int height(NodeAVL node) {
+        if (node == null) return 0;
+        return 1 + Math.max(height((NodeAVL) node.left), height((NodeAVL) node.right));
     }
-    return node;
-  }
 
-  private NodeAVL<E> balanceOnRemoveRight(NodeAVL<E> node) {
-    switch (node.bf) {
-    case 1: node.bf = 0; break;
-    case 0: node.bf = -1; heightChanged = false; break;
-    case -1:
-      NodeAVL<E> leftChild = (NodeAVL<E>) node.left;
-      int bfl = leftChild.bf;
-      node = balanceToRight(node);
-      if (bfl == 0) heightChanged = false;
-      break;
+    private void printGivenLevel(NodeAVL node, int level) {
+        if (node == null) return;
+        if (level == 0) {
+            System.out.print(node.data + "(" + node.bf + ") ");
+        } else if (level > 0) {
+            printGivenLevel((NodeAVL) node.left, level - 1);
+            printGivenLevel((NodeAVL) node.right, level - 1);
+        }
     }
-    return node;
-  }
 
-  private NodeAVL<E> balanceToLeft(NodeAVL<E> node) {
-    NodeAVL<E> hijo = (NodeAVL<E>) node.right;
-    if (hijo.bf == 1) { // RSL
-      node.bf = 0;
-      hijo.bf = 0;
-      return rotateSL(node);
-    } else if (hijo.bf == -1) { // RDL
-      NodeAVL<E> nieto = (NodeAVL<E>) hijo.left;
-      if (nieto.bf == 1) { node.bf = -1; hijo.bf = 0; }
-      else if (nieto.bf == -1) { node.bf = 0; hijo.bf = 1; }
-      else { node.bf = 0; hijo.bf = 0; }
-      nieto.bf = 0;
-      node.right = rotateSR(hijo);
-      return rotateSL(node);
-    } else { // hijo.bf == 0 (Caso de eliminación)
-      node.bf = 1;
-      hijo.bf = -1;
-      return rotateSL(node);
+    public void preOrder() {
+        preOrderRec((NodeAVL) root);
+        System.out.println();
     }
-  }
 
-  private NodeAVL<E> balanceToRight(NodeAVL<E> node) {
-    NodeAVL<E> hijo = (NodeAVL<E>) node.left;
-    if (hijo.bf == -1) { // RSR
-      node.bf = 0;
-      hijo.bf = 0;
-      return rotateSR(node);
-    } else if (hijo.bf == 1) { // RDR
-      NodeAVL<E> nieto = (NodeAVL<E>) hijo.right;
-      if (nieto.bf == -1) { node.bf = 1; hijo.bf = 0; }
-      else if (nieto.bf == 1) { node.bf = 0; hijo.bf = -1; }
-      else { node.bf = 0; hijo.bf = 0; }
-      nieto.bf = 0;
-      node.left = rotateSL(hijo);
-      return rotateSR(node);
-    } else { // hijo.bf == 0 (Caso de eliminación)
-      node.bf = -1;
-      hijo.bf = 1;
-      return rotateSR(node);
+    private void preOrderRec(NodeAVL node) {
+        if (node != null) {
+            System.out.print(node.data + "(" + node.bf + ") ");
+            preOrderRec((NodeAVL) node.left);
+            preOrderRec((NodeAVL) node.right);
+        }
     }
-  }
 
-  private NodeAVL<E> rotateSL(NodeAVL<E> node) {
-    NodeAVL<E> hijo = (NodeAVL<E>) node.right;
-    node.right = hijo.left;
-    hijo.left = node;
-    return hijo;
-  }
-
-  private NodeAVL<E> rotateSR(NodeAVL<E> node) {
-    NodeAVL<E> hijo = (NodeAVL<E>) node.left;
-    node.left = hijo.right;
-    hijo.right = node;
-    return hijo;
-  }
-
-  public void inOrder() {
-    inOrderRec((NodeAVL<E>) root);
-    System.out.println();
-  }
-
-  private void inOrderRec(NodeAVL<E> node) {
-    if (node != null) {
-      inOrderRec((NodeAVL<E>) node.left);
-      System.out.print(node.data + "(" + node.bf + ") ");
-      inOrderRec((NodeAVL<E>) node.right);
+    public void inOrder() {
+        inOrderRec((NodeAVL) root);
+        System.out.println();
     }
-  }
 
-  // ==========================================
-  // EJERCICIOS 4 Y 5: RECORRIDO POR AMPLITUD RECURSIVO
-  // ==========================================
-  public void breadthFirst() {
-    int h = height((NodeAVL<E>) root);
-    for (int i = 1; i <= h; i++) {
-      printGivenLevel((NodeAVL<E>) root, i);
+    private void inOrderRec(NodeAVL node) {
+        if (node != null) {
+            inOrderRec((NodeAVL) node.left);
+            System.out.print(node.data + "(" + node.bf + ") ");
+            inOrderRec((NodeAVL) node.right);
+        }
     }
-    System.out.println();
-  }
-
-  private int height(NodeAVL<E> node) {
-    if (node == null) return 0;
-    return 1 + Math.max(height((NodeAVL<E>) node.left), height((NodeAVL<E>) node.right));
-  }
-
-  private void printGivenLevel(NodeAVL<E> node, int level) {
-    if (node == null) return;
-    if (level == 1) {
-      System.out.print(node.data + "(" + node.bf + ") ");
-    } else if (level > 1) {
-      printGivenLevel((NodeAVL<E>) node.left, level - 1);
-      printGivenLevel((NodeAVL<E>) node.right, level - 1);
-    }
-  }
-
-  // ==========================================
-  // EJERCICIO 6: RECORRIDO EN PREORDEN
-  // ==========================================
-  public void preOrder() {
-    preOrderRec((NodeAVL<E>) root);
-    System.out.println();
-  }
-
-  private void preOrderRec(NodeAVL<E> node) {
-    if (node != null) {
-      System.out.print(node.data + "(" + node.bf + ") ");
-      preOrderRec((NodeAVL<E>) node.left);
-      preOrderRec((NodeAVL<E>) node.right);
-    }
-  }
 }
