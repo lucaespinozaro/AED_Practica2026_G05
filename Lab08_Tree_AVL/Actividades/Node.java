781b673b1495fb543fc01c1f9e6d5ec4b56a7f63
saluhera2023 <saluhera2023@gmail.com>
2026-05-17 17:19:14 -0500

    feat(Act3 y Ejer3-6): Añadido la Actividad 3 y dentro incluye los ejercicios 3 al 6

--- Lab08_Tree_AVL/Actividades/Node.java
+++ Lab08_Tree_AVL/Actividades/Node.java
@@ -1,12 +1,11 @@
-public class Node<E>
-{
-  protected E data;
-  protected Node<E> left;
-  protected Node<E> right;
+public class Node<E> {
+    protected E data;
+    protected Node<E> left;
+    protected Node<E> right;
 
-  public Node(E data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
+    public Node(E data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
