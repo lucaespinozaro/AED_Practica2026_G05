3f77b54f15866ec9ee89ad74fbc6c84a1d32826a
saluhera2023 <saluhera2023@gmail.com>
2026-06-05 23:17:20 -0500

    Update BNode.java

--- Lab09_B-Tree/Actividades/BNode.java
+++ Lab09_B-Tree/Actividades/BNode.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 
 public class BNode<E> {
+    private static int idCounter = 1;
+    private int idNode;
     protected ArrayList<E> keys;
     protected ArrayList<BNode<E>> childs;
     protected int count;
 
     public BNode(int n) {
+        this.idNode = idCounter++;
         this.keys = new ArrayList<E>(n);
         this.childs = new ArrayList<BNode<E>>(n);
         this.count = 0;
@@ -13,6 +16,10 @@ public class BNode<E> {
             this.keys.add(null);
             this.childs.add(null);
         }
+    }
+
+    public int getIdNode() {
+        return this.idNode;
     }
 
     public boolean nodeFull(int maxKeys) {
