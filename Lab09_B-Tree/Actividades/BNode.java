3dab2d81fa481b0b16e72256c3091c4692ae08d4
saluhera2023 <saluhera2023@gmail.com>
2026-06-09 09:11:29 -0500

    Update BNode.java
    Refactor searchNode method for improved readability.

--- Lab09_B-Tree/Actividades/BNode.java
+++ Lab09_B-Tree/Actividades/BNode.java
@@ -32,19 +32,17 @@ public class BNode<E> {
 
     public boolean searchNode(E key, int[] pos) {
         pos[0] = 0;
-        while (pos[0] < count && ((Comparable<E>) key).compareTo(keys.get(pos[0])) > 0) {
+        Comparable<E> compKey = (Comparable<E>) key;
+        
+        while (pos[0] < count && compKey.compareTo(keys.get(pos[0])) > 0) {
             pos[0]++;
         }
-        if (pos[0] < count && ((Comparable<E>) key).compareTo(keys.get(pos[0])) == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return pos[0] < count && compKey.compareTo(keys.get(pos[0])) == 0;
     }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("[");
+        sb.append("Node ID: ").append(idNode).append(" Keys: [");
         for (int i = 0; i < count; i++) {
             sb.append(keys.get(i));
             if (i < count - 1) {
