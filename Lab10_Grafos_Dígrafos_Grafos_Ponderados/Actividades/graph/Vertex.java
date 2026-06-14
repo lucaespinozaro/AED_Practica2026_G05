58dae56a75f0462704588ab7a2050c6536dd0c99
saluhera2023 <saluhera2023@gmail.com>
2026-06-13 20:16:16 -0500

    Update Vertex.java

--- Lab10_Grafos_Dígrafos_Grafos_Ponderados/Actividades/graph/Vertex.java
+++ Lab10_Grafos_Dígrafos_Grafos_Ponderados/Actividades/graph/Vertex.java
@@ -1,1 +1,20 @@
+public class Vertex<E extends Comparable<E>> {
+    private E data;
 
+    public Vertex(E data) {
+        this.data = data;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
