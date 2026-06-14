031b2fd364f9395602bc08f8f7f2bdd48ad7ff91
saluhera2023 <saluhera2023@gmail.com>
2026-06-13 20:13:26 -0500

    Update AdjList.java

--- Lab10_Grafos_Dígrafos_Grafos_Ponderados/Actividades/graph/AdjList.java
+++ Lab10_Grafos_Dígrafos_Grafos_Ponderados/Actividades/graph/AdjList.java
@@ -1,1 +1,24 @@
+import listlinked.ListLinked;
 
+public class AdjList<E extends Comparable<E>> implements Comparable<AdjList<E>> {
+    private Vertex<E> vertex;
+    private ListLinked<Edge<E>> edges;
+
+    public AdjList(Vertex<E> vertex) {
+        this.vertex = vertex;
+        this.edges = new ListLinked<>();
+    }
+
+    public Vertex<E> getVertex() {
+        return vertex;
+    }
+
+    public ListLinked<Edge<E>> getEdges() {
+        return edges;
+    }
+
+    @Override
+    public int compareTo(AdjList<E> o) {
+        return this.vertex.getData().compareTo(o.vertex.getData());
+    }
+}
