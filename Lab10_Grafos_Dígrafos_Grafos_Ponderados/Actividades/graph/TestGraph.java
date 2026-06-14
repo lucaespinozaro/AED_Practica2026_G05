11e99fc2362e0970bfad34b6522ce56e10155a8a
saluhera2023 <saluhera2023@gmail.com>
2026-06-13 20:15:41 -0500

    Update TestGraph.java

--- Lab10_Grafos_Dígrafos_Grafos_Ponderados/Actividades/graph/TestGraph.java
+++ Lab10_Grafos_Dígrafos_Grafos_Ponderados/Actividades/graph/TestGraph.java
@@ -1,1 +1,31 @@
+public class TestGraph {
+    public static void main(String[] args) {
+        GraphLink<String> g = new GraphLink<>();
 
+        g.insertVertex("A");
+        g.insertVertex("B");
+        g.insertVertex("C");
+        g.insertVertex("D");
+
+        g.insertEdge("A", "B");
+        g.insertEdge("A", "C");
+        g.insertEdge("B", "D");
+
+        System.out.println("--- Grafo Inicial ---");
+        System.out.println(g);
+
+        System.out.print("Recorrido DFS desde A: ");
+        g.DFS("A");
+
+        System.out.print("Recorrido BFS desde A: ");
+        g.BFS("A");
+
+        System.out.println("\n--- Eliminando arista (A, C) ---");
+        g.removeEdge("A", "C");
+        System.out.println(g);
+
+        System.out.println("--- Eliminando vértice B (Eficiente) ---");
+        g.removeVertex("B");
+        System.out.println(g);
+    }
+}
