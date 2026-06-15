362155cdd4970572f007617cc28c2e777574083a
saluhera2023 <saluhera2023@gmail.com>
2026-06-15 10:08:51 -0500

    Update StackLink.java

--- Lab10_Grafos_Dígrafos_Grafos_Ponderados/Ejercicios/stacklink/StackLink.java
+++ Lab10_Grafos_Dígrafos_Grafos_Ponderados/Ejercicios/stacklink/StackLink.java
@@ -1,1 +1,35 @@
+package Pilas;
 
+public class StackLink<E extends Comparable<E>> {
+    private Node<E> tope;
+
+    public StackLink() {
+        this.tope = null;
+    }
+
+    public void push(E x) {
+        Node<E> n = new Node<>(x);
+        n.next = tope;
+        tope = n;
+    }
+
+    public E pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Pila vacia");
+        }
+        E dato = tope.dato;
+        tope = tope.next;
+        return dato;
+    }
+
+    public E top() {
+        if (isEmpty()) {
+            throw new RuntimeException("Pila vacia");
+        }
+        return tope.dato;
+    }
+
+    public boolean isEmpty() {
+        return tope == null;
+    }
+}
