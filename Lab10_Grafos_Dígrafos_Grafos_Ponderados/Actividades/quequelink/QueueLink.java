27e1905f5427534a9278a086bf347f23aaecefdd
saluhera2023 <saluhera2023@gmail.com>
2026-06-15 10:13:15 -0500

    Update QueueLink.java

--- Lab10_Grafos_Dígrafos_Grafos_Ponderados/Actividades/quequelink/QueueLink.java
+++ Lab10_Grafos_Dígrafos_Grafos_Ponderados/Actividades/quequelink/QueueLink.java
@@ -1,1 +1,45 @@
+public class QueueLink<E> {
+    private static class Node<E> {
+        E dato;
+        Node<E> next;
 
+        public Node(E dato) {
+            this.dato = dato;
+            this.next = null;
+        }
+    }
+
+    private Node<E> inicio;
+    private Node<E> fin;
+
+    public QueueLink() {
+        this.inicio = null;
+        this.fin = null;
+    }
+
+    public boolean isEmpty() {
+        return inicio == null;
+    }
+
+    public void enqueue(E x) {
+        Node<E> n = new Node<>(x);
+        if (isEmpty()) {
+            inicio = fin = n;
+        } else {
+            fin.next = n;
+            fin = n;
+        }
+    }
+
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Cola vacia");
+        }
+        E d = inicio.dato;
+        inicio = inicio.next;
+        if (inicio == null) {
+            fin = null;
+        }
+        return d;
+    }
+}
