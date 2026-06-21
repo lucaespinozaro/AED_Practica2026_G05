e96829f27963307f88f1674a8eb1ab1a4ca14504
saluhera2023 <saluhera2023@gmail.com>
2026-06-21 14:37:42 -0500

    Update Ejercicio01.java

--- Lab11_Hashing-Dispersion/Ejercicios/Ejercicio01/Ejercicio01.java
+++ Lab11_Hashing-Dispersion/Ejercicios/Ejercicio01/Ejercicio01.java
@@ -1,1 +1,23 @@
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        int[] table = new int[11];
+        for (int i = 0; i < table.length; i++) {
+            table[i] = -1;
+        }
 
+        int[] values = {3, 14, 25, 36, 47, 58};
+
+        for (int val : values) {
+            int index = val % 11;
+            table[index] = val;
+        }
+
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] == -1) {
+                System.out.println(i + ": vacio");
+            } else {
+                System.out.println(i + ": " + table[i]);
+            }
+        }
+    }
+}
