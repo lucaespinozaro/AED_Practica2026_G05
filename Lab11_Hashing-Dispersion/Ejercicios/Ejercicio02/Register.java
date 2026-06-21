2a5495bc70d540dac4020a81e39e844a651c2e84
saluhera2023 <saluhera2023@gmail.com>
2026-06-21 14:34:54 -0500

    Update Register.java

--- Lab11_Hashing-Dispersion/Ejercicios/Ejercicio02/Register.java
+++ Lab11_Hashing-Dispersion/Ejercicios/Ejercicio02/Register.java
@@ -1,1 +1,40 @@
+public class Register<E> implements Comparable<Register<E>> {
+    private int key;
+    private E name;
 
+    public Register(int key, E name) {
+        this.key = key;
+        this.name = name;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public E getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Register<E> other) {
+        return Integer.compare(this.key, other.key);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Register<?> other = (Register<?>) obj;
+        return this.key == other.key;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(key);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + key + ", " + name + ")";
+    }
+}
