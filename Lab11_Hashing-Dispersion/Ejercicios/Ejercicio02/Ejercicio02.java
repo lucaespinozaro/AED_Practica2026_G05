55b3936ef66fac1b93f7a77b0b4cac459ad5939a
saluhera2023 <saluhera2023@gmail.com>
2026-06-21 14:33:14 -0500

    Update Ejercicio02.java

--- Lab11_Hashing-Dispersion/Ejercicios/Ejercicio02/Ejercicio02.java
+++ Lab11_Hashing-Dispersion/Ejercicios/Ejercicio02/Ejercicio02.java
@@ -1,1 +1,25 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int[] values = {10, 17, 24, 31, 4};
 
+        System.out.println("=== Sondeo Lineal ===");
+        HashC<String> hashLinear = new HashC<>(7);
+        for (int val : values) {
+            int probes = hashLinear.insert(new Register<>(val, String.valueOf(val)), false);
+            System.out.println("Valor " + val + " -> Posiciones adicionales exploradas: " + probes);
+        }
+
+        System.out.println("\nEstado final Tabla Lineal:");
+        hashLinear.printTable();
+
+        System.out.println("\n=== Sondeo Cuadratico ===");
+        HashC<String> hashQuad = new HashC<>(7);
+        for (int val : values) {
+            int probes = hashQuad.insert(new Register<>(val, String.valueOf(val)), true);
+            System.out.println("Valor " + val + " -> Posiciones adicionales exploradas: " + probes);
+        }
+
+        System.out.println("\nEstado final Tabla Cuadratica:");
+        hashQuad.printTable();
+    }
+}
