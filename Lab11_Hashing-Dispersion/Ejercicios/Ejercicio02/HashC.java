0cf754c4b3463bb110a8bed32f4b06800fd06143
Luis Santiago Hermoza Ramos <saluhera2023@gmail.com>
2026-06-28 14:25:49 -0500

    Update HashC.java
    Refactor hash table implementation to use marks instead of availability flags. Introduce linear and quadratic probing methods for collision resolution.

--- Lab11_Hashing-Dispersion/Ejercicios/Ejercicio02/HashC.java
+++ Lab11_Hashing-Dispersion/Ejercicios/Ejercicio02/HashC.java
@@ -1,11 +1,11 @@
 public class HashC<E> {
     private static class Element<E> {
         Register<E> register;
-        boolean isAvailable;
+        int mark;
 
         public Element() {
             this.register = null;
-            this.isAvailable = true;
+            this.mark = 0;
         }
     }
 
@@ -25,77 +25,95 @@ public class HashC<E> {
         return Math.abs(key) % size;
     }
 
+    private int[] linearProbing(int key, boolean forInsert) {
+        int baseIndex = hash(key);
+        int index = baseIndex;
+        int firstDeleted = -1;
+        int probes = 0;
+        do {
+            if (table[index].mark == 0) {
+                if (forInsert) {
+                    int target = (firstDeleted != -1) ? firstDeleted : index;
+                    return new int[]{target, probes};
+                }
+                return new int[]{-1, probes};
+            }
+            if (table[index].mark == -1) {
+                if (forInsert && firstDeleted == -1) {
+                    firstDeleted = index;
+                }
+            } else if (table[index].mark == 1 && table[index].register.getKey() == key) {
+                return new int[]{index, probes};
+            }
+            index = (index + 1) % size;
+            probes++;
+        } while (index != baseIndex);
+
+        if (forInsert && firstDeleted != -1) {
+            return new int[]{firstDeleted, probes};
+        }
+        return new int[]{-1, probes};
+    }
+
+    private int[] quadraticProbing(int key, boolean forInsert) {
+        int baseIndex = hash(key);
+        int firstDeleted = -1;
+        int probes = 0;
+        for (int i = 0; i < size; i++) {
+            int index = (baseIndex + i * i) % size;
+            if (table[index].mark == 0) {
+                if (forInsert) {
+                    int target = (firstDeleted != -1) ? firstDeleted : index;
+                    return new int[]{target, probes};
+                }
+                return new int[]{-1, probes};
+            }
+            if (table[index].mark == -1) {
+                if (forInsert && firstDeleted == -1) {
+                    firstDeleted = index;
+                }
+            } else if (table[index].mark == 1 && table[index].register.getKey() == key) {
+                return new int[]{index, probes};
+            }
+            if (i > 0) probes++;
+        }
+        if (forInsert && firstDeleted != -1) {
+            return new int[]{firstDeleted, probes};
+        }
+        return new int[]{-1, probes};
+    }
+
     public int insert(Register<E> reg, boolean isQuadratic) {
         if (reg == null) return 0;
-        int baseIndex = hash(reg.getKey());
-        int i = 0;
-        int probes = 0;
-        int targetIndex = -1;
-
-        do {
-            int index = (baseIndex + (isQuadratic ? i * i : i)) % size;
-            if (i > 0) probes++;
-
-            if (table[index].register == null) {
-                if (targetIndex == -1) targetIndex = index;
-                break;
-            }
-            if (table[index].isAvailable) {
-                if (targetIndex == -1) targetIndex = index;
-            } else if (table[index].register.getKey() == reg.getKey()) {
-                table[index].register = reg;
-                return probes;
-            }
-            i++;
-        } while (i < size);
-
+        int[] probeResult = isQuadratic ? quadraticProbing(reg.getKey(), true) : linearProbing(reg.getKey(), true);
+        int targetIndex = probeResult[0];
+        int probes = probeResult[1];
         if (targetIndex != -1) {
-            table[targetIndex].register = reg;
-            table[targetIndex].isAvailable = false;
+            if (table[targetIndex].mark == 1 && table[targetIndex].register.getKey() == reg.getKey()) {
+                table[targetIndex].register = reg;
+            } else {
+                table[targetIndex].register = reg;
+                table[targetIndex].mark = 1;
+            }
         } else {
             System.out.println("Error: Tabla hash llena");
         }
         return probes;
     }
 
-    public Register<E> search(int key, boolean isQuadratic) {
-        int baseIndex = hash(key);
-        int i = 0;
-        do {
-            int index = (baseIndex + (isQuadratic ? i * i : i)) % size;
-            if (table[index].register == null) {
-                return null;
-            }
-            if (!table[index].isAvailable && table[index].register.getKey() == key) {
-                return table[index].register;
-            }
-            i++;
-        } while (i < size);
-        return null;
-    }
-
-    public void delete(int key, boolean isQuadratic) {
-        int baseIndex = hash(key);
-        int i = 0;
-        do {
-            int index = (baseIndex + (isQuadratic ? i * i : i)) % size;
-            if (table[index].register == null) {
-                return;
-            }
-            if (!table[index].isAvailable && table[index].register.getKey() == key) {
-                table[index].isAvailable = true;
-                return;
-            }
-            i++;
-        } while (i < size);
+    public void clearTable() {
+        for (int i = 0; i < size; i++) {
+            table[i].register = null;
+            table[i].mark = 0;
+        }
     }
 
     public void printTable() {
         for (int i = 0; i < size; i++) {
-            if (table[i].register != null && !table[i].isAvailable) {
-                System.out.println(i + ": " + table[i].register);
+            if (table[i].mark == 1) {
+                System.out.println("  Indice " + i + ": " + table[i].register);
             } else {
-                System.out.println(i + ": vacio");
+                System.out.println("  Indice " + i + ": vacio");
             }
         }
     }
