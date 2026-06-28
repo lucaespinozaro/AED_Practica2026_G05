218def72ae9c28067d8955a5305d5d2d42575e22
Luis Santiago Hermoza Ramos <saluhera2023@gmail.com>
2026-06-28 13:47:50 -0500

    Update HashC.java

--- Lab11_Hashing-Dispersion/Actividades/Actividad02/HashC.java
+++ Lab11_Hashing-Dispersion/Actividades/Actividad02/HashC.java
@@ -1,11 +1,11 @@
 public class HashC<E> {
     private static class Element<E> {
         Register<E> register;
-        boolean isAvailable;
+        int mark;
 
         public Element() {
             this.register = null;
-            this.isAvailable = true;
+            this.mark = 0;
         }
     }
 
@@ -25,67 +25,75 @@ public class HashC<E> {
         return Math.abs(key) % size;
     }
 
-    public void insert(Register<E> reg) {
-        if (reg == null) return;
-        int index = hash(reg.getKey());
-        int start = index;
-        int targetIndex = -1;
+    private int[] linearProbing(int key, boolean forInsert) {
+        int baseIndex = hash(key);
+        int index = baseIndex;
+        int firstDeleted = -1;
+        int probes = 0;
         do {
-            if (table[index].register == null) {
-                if (targetIndex == -1) targetIndex = index;
-                break;
+            if (table[index].mark == 0) {
+                if (forInsert) {
+                    int target = (firstDeleted != -1) ? firstDeleted : index;
+                    return new int[]{target, probes};
+                }
+                return new int[]{-1, probes};
             }
-            if (table[index].isAvailable) {
-                if (targetIndex == -1) targetIndex = index;
-            } else if (table[index].register.getKey() == reg.getKey()) {
-                table[index].register = reg;
-                return;
+            if (table[index].mark == -1) {
+                if (forInsert && firstDeleted == -1) {
+                    firstDeleted = index;
+                }
+            } else if (table[index].mark == 1 && table[index].register.getKey() == key) {
+                return new int[]{index, probes};
             }
             index = (index + 1) % size;
-        } while (index != start);
+            probes++;
+        } while (index != baseIndex);
 
+        if (forInsert && firstDeleted != -1) {
+            return new int[]{firstDeleted, probes};
+        }
+        return new int[]{-1, probes};
+    }
+
+    public void insert(Register<E> reg) {
+        if (reg == null) return;
+        int[] probeResult = linearProbing(reg.getKey(), true);
+        int targetIndex = probeResult[0];
         if (targetIndex != -1) {
-            table[targetIndex].register = reg;
-            table[targetIndex].isAvailable = false;
+            if (table[targetIndex].mark == 1 && table[targetIndex].register.getKey() == reg.getKey()) {
+                table[targetIndex].register = reg;
+            } else {
+                table[targetIndex].register = reg;
+                table[targetIndex].mark = 1;
+            }
         } else {
             System.out.println("Error: Tabla hash llena");
         }
     }
 
     public Register<E> search(int key) {
-        int index = hash(key);
-        int start = index;
-        do {
-            if (table[index].register == null) {
-                return null;
-            }
-            if (!table[index].isAvailable && table[index].register.getKey() == key) {
-                return table[index].register;
-            }
-            index = (index + 1) % size;
-        } while (index != start);
+        int[] probeResult = linearProbing(key, false);
+        int index = probeResult[0];
+        if (index != -1 && table[index].mark == 1) {
+            return table[index].register;
+        }
         return null;
     }
 
     public void delete(int key) {
-        int index = hash(key);
-        int start = index;
-        do {
-            if (table[index].register == null) {
-                return;
-            }
-            if (!table[index].isAvailable && table[index].register.getKey() == key) {
-                table[index].isAvailable = true;
-                return;
-            }
-            index = (index + 1) % size;
-        } while (index != start);
+        int[] probeResult = linearProbing(key, false);
+        int index = probeResult[0];
+        if (index != -1 && table[index].mark == 1) {
+            table[index].mark = -1;
+        }
     }
 
     public void printTable() {
         for (int i = 0; i < size; i++) {
-            if (table[i].register != null && !table[i].isAvailable) {
+            if (table[i].mark == 1) {
                 System.out.println(i + ": " + table[i].register);
+            } else if (table[i].mark == -1) {
+                System.out.println(i + ": eliminado");
             } else {
                 System.out.println(i + ": vacio");
             }
